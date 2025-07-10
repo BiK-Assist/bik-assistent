7d02eab8b088cb5a51b4959556b36e579f6dca62
BiK Solution <bik@Laptop-von-BiK.local>
2025-07-10 14:11:11 +0200

    fix: set port for Render deployment

--- app.py
+++ app.py
@@ -18,5 +18,6 @@ def webhook():
 def hello():
     return "BiK Assistent läuft."
 
-if __name__ == "__main__":
-    app.run()
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
